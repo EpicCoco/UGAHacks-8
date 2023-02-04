59942bfed9c0cb8871d2d79e28eb64e7a9b0caf2
EpicCoco <forthecodeman@gmail.com>
2023-02-04 15:46:18 -0500

    changes lots

--- Hacks8/src/App.java
+++ Hacks8/src/App.java
@@ -1,9 +1,14 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -14,7 +19,8 @@ import java.io.InputStream;
  
 public class App extends Application {
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) {
+        /* 
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -27,17 +33,42 @@ public class App extends Application {
         
         StackPane root = new StackPane();
         root.getChildren().add(btn);
+        */
+        /*
+        Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+        Scene scene = new Scene(root);
+        */
+        /*
+        WebView browser = new WebView();
+        WebEngine webEngine = browser.getEngine();
+        webEngine.load("http://youtube.com");
+
+        Scene scene = new Scene(root, 400, 800);
   
-  /*
-  Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
-  Scene scene = new Scene(root);
-  */
-  
-    Scene scene = new Scene(root, 400, 800);
-  
-    primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
+        */
+    
+        stage.setTitle("HTML");
+        stage.setWidth(500);
+        stage.setHeight(500);
+        Scene scene = new Scene(new Group());
+
+        VBox root = new VBox();     
+
+        final WebView browser = new WebView();
+        final WebEngine webEngine = browser.getEngine();
+
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(browser);
+        webEngine.loadContent("<b>asdf</b>");
+
+        root.getChildren().addAll(scrollPane);
+        scene.setRoot(root);
+
+        stage.setScene(scene);
+        stage.show();
     } //start
  
     public static void main(String[] args) {

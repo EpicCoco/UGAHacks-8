e4f410978fe2a54e31f86516b008ae38a177957c
EpicCoco <forthecodeman@gmail.com>
2023-02-04 12:12:50 -0500

    Modified App, added Task

--- Hacks8/src/App.java
+++ Hacks8/src/App.java
@@ -4,11 +4,23 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
  
 public class App extends Application {
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        String filePath = new File("").getAbsolutePath();
+        filePath.concat("lib/home_screen_map.PNG");
+        InputStream stream = new FileInputStream("C:\\Users\\Codey\\git\\UGAHacks-8\\Hacks8\\lib\\home_screen_map.PNG");
+        Image image = new Image(stream);
+        ImageView imageView = new ImageView(image);
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -19,15 +31,17 @@ public class App extends Application {
             }
         });
         
-        StackPane root = new StackPane();
+        //StackPane root = new StackPane();
+        VBox root = new VBox();
         root.getChildren().add(btn);
+        root.getChildren().add(imageView);
   
   /*
   Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
   Scene scene = new Scene(root);
   */
   
-  Scene scene = new Scene(root, 300, 250);
+  Scene scene = new Scene(root, 400, 800);
   
   primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);

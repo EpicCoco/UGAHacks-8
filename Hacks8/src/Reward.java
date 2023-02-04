e4b20ede0872b9f37f775d36b25dda943bab7281
TheKingChicken <tylerjsimile@gmail.com>
2023-02-04 13:48:38 -0500

    Reward Comnpleted

--- Hacks8/src/Reward.java
+++ Hacks8/src/Reward.java
@@ -1,15 +1,32 @@
 public class Reward {
     int xp;
     String name;
+    int rewAmount;
 
 
     public int getXP() {
         return xp;
     } //getXP
 
+    public void setXP(int i) {
+        xp = i;
+    } //setXP
+
     public String getName() {
         return name;
     } //getName
+
+    public void setName(String s)  {
+        name = s;
+    } //setName
+
+    public int getAmount() {
+        return rewAmount;
+    } //getAmount
+
+    public void setAmount(int i) {
+        rewAmount = i;
+    } //setAmount
 
     public String toString() {
         return (name + " " + xp);

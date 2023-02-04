627652d59f209fd58f9c70bec60748af10086a97
EpicCoco <forthecodeman@gmail.com>
2023-02-04 12:25:44 -0500

    added Log

---
+++ Hacks8/src/Log.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Log {
+    ArrayList<Quest> quests = new ArrayList<Quest>();
+
+    public void addQuest(Quest quest) {
+        quests.add(quest);
+    } //addQuest
+} //Log
